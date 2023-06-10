b3621229717586117326c7065ad6360eac41ed7c
Səmədov İsmət <ismetsemedov@gmail.com>
2023-06-10 22:28:19 +0400

    check

---
+++ Classes/Human.java
@@ -0,0 +1,18 @@
+public class Human {
+    public int Age;
+    public double Height;
+    public double Weight;
+    public String Name;
+
+
+    public Human() {
+    }
+
+    public Human(int Age, double Height, double Weight, String Name) {
+        this.Age = Age;
+        this.Height = Height;
+        this.Weight = Weight;
+        this.Name = Name;
+
+    }
+}
