94ed63c02dd8d28bb22c3ec278390ef62d610374
Səmədov İsmət <ismetsemedov@gmail.com>
2023-06-11 14:16:54 +0400

    check

--- Classes/Human.java
+++ Classes/Human.java
@@ -16,4 +16,20 @@ public class Human {
         this.Weight = Weight;
         this.Name = Name;
     }
+
+    public void sleeping() {
+        System.out.println(Name + " is sleeping");
+    }
+
+    public void walking() {
+        System.out.println(Name + " is walking");
+    }
+
+    public void jumping() {
+        System.out.println(Name + " is jumping");
+    }
+
+    public void atGym() {
+        System.out.println(Name + " at gym");
+    }
 }
--- Classes/HumanApp.java
+++ Classes/HumanApp.java
@@ -3,6 +3,11 @@ package Classes;
 public class HumanApp {
     public static void main(String[] args) {
         Human empty= new Human();
+        System.out.println(empty);
+        Human IsmatS= new Human(29,1.75,85.9,"Ismat");
+        Human QismatS =new Human(27,78.0,1.87,"Qismat");
+        IsmatS.sleeping();
+        QismatS.walking();
     }
 
 
