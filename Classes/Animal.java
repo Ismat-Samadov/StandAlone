abba175a3fe654eefec1b5b9534f216649614123
Səmədov İsmət <ismetsemedov@gmail.com>
2023-06-17 13:39:07 +0400

    check

--- Classes/Animal.java
+++ Classes/Animal.java
@@ -20,13 +20,30 @@ public class Animal {
     public void setSpecies(String species) {
         this.species = species;
     }
-    public double getWeight(){
+
+    public double getWeight() {
         return weight;
     }
-    public void setWeight(double weight){
-        this.weight=weight;
+
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
 
