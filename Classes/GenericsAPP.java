5cd123c1ca633af540927cd48e79a7ae13ba2887
Səmədov İsmət <ismetsemedov@gmail.com>
2023-06-27 11:21:25 +0400

    check

--- Classes/GenericsAPP.java
+++ Classes/GenericsAPP.java
@@ -1,4 +1,14 @@
 package Classes;
 
 public class GenericsAPP {
+
+    public static void requireNonNull(String name){
+        if (name==null){
+            throw new NullPointerException("Please Enter Name");
+        }
+    }
+
+
+
+
 }
